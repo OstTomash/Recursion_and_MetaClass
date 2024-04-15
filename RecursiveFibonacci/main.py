a2ced167374311857e98b8f4d6304075e58917d4
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2024-04-15 16:31:46 +0300

    Fix recursive func

--- RecursiveFibonacci/main.py
+++ RecursiveFibonacci/main.py
@@ -1,6 +1,10 @@
 def recursive_fibonacci(n):
-    if n in (0, 1):
-        return n
+    if n <= 0:
+        return "Wrong input"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
 
     return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
 
